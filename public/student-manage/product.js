9c86cd83280833eb4edcaed5a8da5818c45fe47a
Lâm Nguyễn Bá <nguyenbalam2307@gmail.com>
2022-12-30 17:25:53 +0700

    uphome

--- public/student-manage/product.js
+++ public/student-manage/product.js
@@ -11,4 +11,36 @@ class Product {
         this.name = name;
         this.price = price;
     }
+
+    getId() {
+        return this.id;
+    }
+
+    setId(id) {
+        this.id = id;
+    }
+
+    getImage() {
+        return this.image;
+    }
+
+    setImage(image) {
+        this.image = image;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    setName(name) {
+        this.name = name;
+    }
+
+    getPrice() {
+        return this.price;
+    }
+
+    setPrice(price) {
+        this.price = price;
+    }
 }
